053ecff82f6dbfc5acdedbd6f28ff44f6850516f
Nikita Lebovvskii <lebovvskii.design@ya.ru>
2023-12-20 20:32:55 +1000

    updated routes

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,6 @@ import {
   BuyMeCoffee,
   Title,
 } from "@/components";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
